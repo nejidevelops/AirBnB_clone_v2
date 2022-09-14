18be3cf5207d07924be68c9b227ae3161e487d99
Newton Ombese <newtonombese1@gmail.com>
2022-09-14 22:49:05 +0300

    test console file

---
+++ tests/test_console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Contains the class TestConsoleDocs
+"""
+
+import console
+import inspect
+import pep8
+import unittest
+HBNBCommand = console.HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """Tests for documentation of the console"""
+    pass
