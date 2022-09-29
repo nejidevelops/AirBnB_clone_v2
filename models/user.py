1c4a0497dc38be8a8bdb3c5e187daf9ec57df659
Newton Ombese <newtonombese1@gmail.com>
2022-09-29 17:09:53 +0300

    model user python file

--- models/user.py
+++ models/user.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" Inherits from BaseModel."""
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """
-    Defines class User.
-
+    """This is the class for user
     Attributes:
-        __tablename__ (str): The name of the MySQL table to store users.
-        email: (sqlalchemy String): The user's email address.
-        password (sqlalchemy String): The user's password.
-        first_name (sqlalchemy String): The user's first name.
-        last_name (sqlalchemy String): The user's last name.
-        places (sqlalchemy relationship): The User-Place relationship.
-        reviews (sqlalchemy relationship): The User-Review relationship.
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
     """
-    __tablename__ = "users"
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-    places = relationship("Place", backref="user", cascade="delete")
-    reviews = relationship("Review", backref="user", cascade="delete")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='user',
+                          cascade='all, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                           cascade='all, delete-orphan')
