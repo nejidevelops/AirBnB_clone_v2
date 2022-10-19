b6529d79efa0f5f96ee9354d4c3dcd79a7df3e36
Newton Ombese <newtonombese1@gmail.com>
2022-10-20 02:04:28 +0300

    giving it all

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-""" Script that runs a Flask app """
-from flask import Flask, render_template
+"""Starts a Flask web application"""
+
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def closing(error):
-    """closes session """
-    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states():
-    """ function that returns text"""
-    state = storage.all()
-    return render_template('7-states_list.html', state=state)
+    """Returns a rendered html template
+    at the /states_list route,
+    listing all states"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
